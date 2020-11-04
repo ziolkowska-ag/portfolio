a038779980a96a4369cd1d234e714f8d17228c04
Agnieszka Ziolkowska <a.ziolkowska@commonwombat.pl>
2020-11-05 00:29:04 +0100

    Small cleanup

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,6 +3,7 @@ import Layout from "../shared-components/Layout"
 import ContactContent from "../pages-content/contact/index"
 
 export default function Contact() {
+
   return (
     <Layout title="CONTACT" hideSocials>
       <ContactContent />
