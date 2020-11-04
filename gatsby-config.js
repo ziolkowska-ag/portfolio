077014d6c3767a17bc1341a6813740cc6776d76f
Agnieszka Ziolkowska <a.ziolkowska@commonwombat.pl>
2020-11-04 23:45:23 +0100

    Fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,6 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-transition-link',
-    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-typescript',
       options: {
